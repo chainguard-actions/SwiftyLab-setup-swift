878b94ae51236e13fae646815b0ad64643efeb61
actionchain <actionchain@chainguard.dev>
2026-06-17 12:31:20 +0000

    Update SwiftyLab/setup-swift@v1.12.0

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+import * as core from '@actions/core'
+import {ToolchainVersion, SWIFT_BRANCH_REGEX} from './version'
+import {Swiftorg} from './swiftorg'
+import {SdkSnapshot, ToolchainSnapshot} from './snapshot'
+import {Platform} from './platform'
+
+export async function run() {
+  try {
+    const requestedVersion = core.getInput('swift-version') ?? 'latest'
+    const development = core.getBooleanInput('development')
+    const sdksStr = core.getInput('sdks')
+    const sdks = sdksStr ? sdksStr.split(';') : []
+    const version = ToolchainVersion.create(requestedVersion, development, sdks)
+
+    if (version.requiresSwiftOrg) {
+      core.startGroup('Syncing swift.org data')
+      const checkLatest = core.getInput('check-latest')
+      const submodule = new Swiftorg(checkLatest)
+      await submodule.update()
+      core.endGroup()
+    }
+
+    const dryRun = core.getBooleanInput('dry-run')
+    let snapshot: ToolchainSnapshot
+    let sdkSnapshots: SdkSnapshot[]
+    let installedVersion: string
+    if (dryRun) {
+      const toolchain = await Platform.toolchain(version)
+      if (toolchain) {
+        snapshot = toolchain
+      } else {
+        throw new Error(`No Swift toolchain found for ${version}`)
+      }
+      const match = SWIFT_BRANCH_REGEX.exec(toolchain.branch)
+      if (match && match.length > 1) {
+        installedVersion = match[1]
+      } else {
+        installedVersion = requestedVersion
+      }
+      sdkSnapshots = await version.sdkSnapshots(toolchain)
+    } else {
+      const {installer, sdkInstallers} = await Platform.install(version)
+      snapshot = installer.data
+      sdkSnapshots = sdkInstallers.map(installer => installer.data)
+      installedVersion = await installer.installedSwiftVersion()
+    }
+    core.setOutput('swift-version', installedVersion)
+    core.setOutput('toolchain', JSON.stringify(snapshot))
+    core.setOutput('sdks', JSON.stringify(sdkSnapshots))
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+if (process.env.JEST_WORKER_ID === undefined) {
+  run()
+}
